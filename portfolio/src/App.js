e5d7e10819fe5adf13411aabac700b719713ca48
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-17 09:55:01 -0500

    imported header and footer components to app.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,20 +1,13 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <header>
-          <h1 class="website-name" id="name-of-website">
-            FABIOLA PINA
-          </h1>
-          <nav class="nav-bar" id="navigation">
-            <a href="/html/">HOME</a> | <a href="/css/">ABOUT ME</a> |
-            <a href="/js/">MY PROJECTS</a> | <a href="/python/">TESTIMONIALS</a>{" "}
-            |<a href="/html/">CONTACT ME</a> |<a href="/html/">RESUME</a>
-          </nav>
-        </header>
+        <Header />
         <hr />
         <main>
           <h1>Hello,</h1>
@@ -24,11 +17,7 @@ export default class App extends Component {
           </h3>
         </main>
         <hr />
-        <footer>
-          <p>
-            FIND ME ON GITHUB, LINKEDIN, TWITTER, TIKTOK, INSTAGRAM, AND MEDIUM
-          </p>
-        </footer>
+        <Footer />
       </div>
     );
   }
---
+++ portfolio/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <p>
+          FIND ME ON GITHUB, LINKEDIN, TWITTER, TIKTOK, INSTAGRAM, AND MEDIUM
+        </p>
+      </footer>
+    );
+  }
+}
