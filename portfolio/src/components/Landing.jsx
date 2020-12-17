81a9b1e8fc199f25a6f8616b174e595c743ace89
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-17 11:19:00 -0500

    created landing page component

---
+++ portfolio/src/components/Landing.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <main>
+        <h1>Hello,</h1>
+        <h3>
+          I am a software engineer who seeks to create user-friendly apps and
+          websites.
+        </h3>
+      </main>
+    );
+  }
+}
