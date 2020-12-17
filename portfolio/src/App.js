7608fd5ccf6e6455b34baaf530484c7d27b06ebc
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-17 11:30:09 -0500

    imported react router

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Landing from "./components/Landing";
