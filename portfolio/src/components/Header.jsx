e96f28e57e96f13129e9d474466fea578e249543
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-17 09:52:51 -0500

    created header component

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1 class="website-name" id="name-of-website">
+          FABIOLA PINA
+        </h1>
+        <nav class="nav-bar" id="navigation">
+          <a href="/html/">HOME</a> | <a href="/css/">ABOUT ME</a> |
+          <a href="/js/">MY PROJECTS</a> | <a href="/python/">TESTIMONIALS</a> |
+          <a href="/html/">CONTACT ME</a> |<a href="/html/">RESUME</a>
+        </nav>
+      </header>
+    );
+  }
+}
