5bc72681e43d32f8240f59b87b8f64c389382eae
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-17 19:48:42 -0500

    created about me component

---
+++ portfolio/src/components/AboutMe.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class AboutMe extends Component {
+  render() {
+    return (
+      <div className="about-page">
+        <h1 id="about-me">Aboout Me</h1>
+      </div>
+    );
+  }
+}
