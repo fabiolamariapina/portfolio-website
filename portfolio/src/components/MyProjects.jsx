c10b3de519303cb774e4003308a87744e0c60570
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-18 13:25:10 -0500

    added loading animations to my projects page

--- portfolio/src/components/MyProjects.jsx
+++ portfolio/src/components/MyProjects.jsx
@@ -3,8 +3,10 @@ import React, { Component } from "react";
 export default class MyProjects extends Component {
   render() {
     return (
-      <div className="my-projects">
-        <h1 className="my-projects-header">My Projects</h1>
+      <div className="projects-page">
+        <h1 className="my-projects" id="my-projects-header">
+          My Projects
+        </h1>
       </div>
     );
   }
