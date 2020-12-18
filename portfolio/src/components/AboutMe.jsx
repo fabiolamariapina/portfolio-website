aff4f88ba364ec5373580863cc3d378ab88f317a
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-17 20:38:29 -0500

    added the content of my about me page

--- portfolio/src/components/AboutMe.jsx
+++ portfolio/src/components/AboutMe.jsx
@@ -18,8 +18,44 @@ export default class AboutMe extends Component {
             immersive. My background in the social sciences, along with my
             community engagement experience, has made me adept at knowing what
             people like and need, which helps me create user-friendly apps and
-            websites. My professional interests are tech policy, diversity and
-            inclusion in tech, and social innovation and entrepreneurship.
+            websites.
+          </p>
+          <p className="about-me" id="about-me-spiel-part-2">
+            My skills include HTML, CSS, JavaScript, React, RESTful API,
+            Node.js, Express, Mongo DB, Mongoose, APIs, Git, Github, PostgreSQL,
+            BCrypt, Heroku, Axios, responsive design, and command lines. I am
+            also in the process of adding Python to my list of skills by taking
+            The Modern Python 3 Bootcamp course on Udemy. My other professional
+            interests outside of software engineering are user experience, data
+            science, tech policy, diversity and inclusion in tech, and social
+            innovation and entrepreneurship.
+          </p>
+          <p className="about-me" id="about-me-spiel-part-3">
+            Outside of work, I love to lift weights, do yoga, inline skate,
+            sing, listen to music, and watch Youtube and documentaries. I am
+            also interested in learning how to play the guitar and re-learning
+            the piano. My other personal goals are to learn how to surf, earn my
+            SCUBA certification, and compete in a powerlifting meet.
+          </p>
+          <p className="about-me" id="about-me-spiel-part-4">
+            You can find me on{" "}
+            <a
+              className="linkedIn-link"
+              href="https://github.com/fabiolamariapina"
+              target="_blank"
+            >
+              {" "}
+              Github{" "}
+            </a>{" "}
+            and{" "}
+            <a
+              className="linkedIn-link"
+              href="https://www.linkedin.com/in/fabiolampina/"
+              target="_blank"
+            >
+              {" "}
+              LinkedIn.{" "}
+            </a>
           </p>
         </div>
       </div>
