4a75281273cec4ee4a5c28b93992c84951298756
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-17 19:37:56 -0500

    added animation to footer when it loads

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -3,28 +3,30 @@ import React, { Component } from "react";
 export default class Footer extends Component {
   render() {
     return (
-      <footer>
-        <p className="find-me">
-          FIND ME ON
-          <a
-            className="linkedIn-link"
-            href="https://github.com/fabiolamariapina"
-            target="_blank"
-          >
-            {" "}
-            GITHUB{" "}
-          </a>
-          AND
-          <a
-            className="linkedIn-link"
-            href="https://www.linkedin.com/in/fabiolampina/"
-            target="_blank"
-          >
-            {" "}
-            LINKEDIN{" "}
-          </a>
-        </p>
-      </footer>
+      <div className="footer">
+        <footer>
+          <p className="find-me">
+            FIND ME ON
+            <a
+              className="linkedIn-link"
+              href="https://github.com/fabiolamariapina"
+              target="_blank"
+            >
+              {" "}
+              GITHUB{" "}
+            </a>
+            AND
+            <a
+              className="linkedIn-link"
+              href="https://www.linkedin.com/in/fabiolampina/"
+              target="_blank"
+            >
+              {" "}
+              LINKEDIN{" "}
+            </a>
+          </p>
+        </footer>
+      </div>
     );
   }
 }
