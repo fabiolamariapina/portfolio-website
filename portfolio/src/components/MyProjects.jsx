e91921505f63ad67a901b422d99035997860b86e
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-18 13:03:09 -0500

    creaated component for my projects page

---
+++ portfolio/src/components/MyProjects.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class MyProjects extends Component {
+  render() {
+    return (
+      <div className="my-projects-page">
+        <h1 className="my=projects">My Projects</h1>
+      </div>
+    );
+  }
+}
