b6e151f4b622dde4f1e2607f65331b5a821a2142
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-18 13:28:45 -0500

    created component for contact me page

---
+++ portfolio/src/components/ContactMe.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class ContactMe extends Component {
+  render() {
+    return (
+      <div className="contact-page">
+        <h1 className="contact-me" id="contact-me-header">
+          My Projects
+        </h1>
+      </div>
+    );
+  }
+}
