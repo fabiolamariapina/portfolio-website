20d9ce0cffba42a41889fe0ad9121dc9d7f588fd
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-19 13:32:06 -0500

    installed axios

--- portfolio/src/components/ContactMe.jsx
+++ portfolio/src/components/ContactMe.jsx
@@ -13,6 +13,7 @@ export default class ContactMe extends Component {
     this.onEmailChange = this.onEmailChange.bind(this);
     this.onMessageChange = this.onMessageChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.resetForm = this.resetForm.bind(this);
   }
   onNameChange(event) {
     this.setState({ name: event.target.value });
@@ -25,6 +26,25 @@ export default class ContactMe extends Component {
   }
   handleSubmit(event) {
     event.preventDefault();
+    // fetch("http://localhost:3000/send", {
+    //   method: "POST",
+    //   body: JSON.stringify(this.state),
+    //   headers: {
+    //     Accept: "application/json",
+    //     "Content-Type": "application/json",
+    //   },
+    // })
+    //   .then((response) => response.json())
+    //   .then((response) => {
+    //     if (response.status === "success") {
+    //       alert("Message Sent.");
+    //       this.resetForm();
+    //     } else if (response.status === "fail") {
+    //       alert("Message failed to send.");
+    //     }
+    //   });
+  }
+  resetForm() {
     this.setState({
       name: "",
       email: "",
