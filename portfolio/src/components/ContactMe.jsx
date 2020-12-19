3dc4a4eed7339909d9df9d5fcad45aea8a5b19e0
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-19 13:16:10 -0500

    began adding functionality to contact form

--- portfolio/src/components/ContactMe.jsx
+++ portfolio/src/components/ContactMe.jsx
@@ -2,6 +2,24 @@ import React, { Component } from "react";
 import Button from "react-bootstrap/Button";
 
 export default class ContactMe extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+    };
+  }
+  onNameChange(event) {
+    this.setState({ name: event.target.value });
+  }
+  onEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
+  onMessageChange(event) {
+    this.setState({ message: event.target.value });
+  }
+  handleSubmit(event) {}
   render() {
     return (
       <div className="contact-page">
@@ -17,17 +35,25 @@ export default class ContactMe extends Component {
           <form
             className="contact-me"
             id="contact-form"
-            // onSubmit={this.handleSubmit.bind(this)}
-            // method="POST"
+            onSubmit={this.handleSubmit.bind(this)}
+            method="POST"
           >
             <div className="form-group">
-              <input type="text" className="form-control" placeholder="Name" />
+              <input
+                type="text"
+                className="form-control"
+                value={this.state.name}
+                onChange={this.onNameChange.bind(this)}
+                placeholder="Name"
+              />
             </div>
             <div className="form-group">
               <input
                 type="email"
                 className="form-control"
                 aria-describedby="emailHelp"
+                value={this.state.email}
+                onChange={this.onEmailChange.bind(this)}
                 placeholder="Email"
               />
             </div>
@@ -36,6 +62,8 @@ export default class ContactMe extends Component {
                 className="form-control"
                 id="message"
                 rows="5"
+                value={this.state.message}
+                onChange={this.onMessageChange.bind(this)}
                 placeholder="Message"
               ></textarea>
             </div>
