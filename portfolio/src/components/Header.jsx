4887b3be665de0a9ae2f06b2771e67b0f9599153
fabiolamariapina <fabiolamariapina@gmail.com>
2020-12-20 12:11:14 -0500

    began process of restyling links in nav bar

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,40 +1,51 @@
 import React, { Component } from "react";
 import Nav from "react-bootstrap/Nav";
-import { BrowserRouter as Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+`;
 
 export default class Header extends Component {
   render() {
     return (
       <div>
         <header className="header">
-          <Link to="/" className="link-to-home-">
-            <h1 className="website-name" id="name-of-website">
-              FABIOLA PINA
-            </h1>
-          </Link>
+          <h1 className="website-name" id="name-of-website">
+            FABIOLA PINA
+          </h1>
           <div className="nav-bar">
             <Nav id="navigation">
-              <Link to="/" className="link-to-home">
+              <Link to="/" style={{ textDecoration: "none" }}>
                 HOME
               </Link>{" "}
               |{" "}
-              <Link to="/about-me" className="link-to-about-me">
+              <Link to="/about-me" style={{ textDecoration: "none" }}>
                 ABOUT ME
               </Link>{" "}
               |{" "}
-              <Link to="/my-projects" className="link-to-my-projects">
+              <Link to="/my-projects" style={{ textDecoration: "none" }}>
                 MY PROJECTS
               </Link>{" "}
               |{" "}
-              <Link to="/contact-me" className="link-to-contact-me">
+              <Link to="/contact-me" style={{ textDecoration: "none" }}>
                 CONTACT ME
               </Link>{" "}
               | <a href="/html/">RESUME</a> |{" "}
-              <Link to="/contact-me" className="link-to-contact-me">
+              <Link to="/contact-me" cstyle={{ textDecoration: "none" }}>
                 GITHUB
               </Link>{" "}
               |{" "}
-              <Link to="/contact-me" className="link-to-contact-me">
+              <Link to="/contact-me" style={{ textDecoration: "none" }}>
                 LINKEDIN
               </Link>
             </Nav>
